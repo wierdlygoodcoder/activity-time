40cf2cf118a7acc371d35c8ba83a56405cfb8a4a
wierdlygoodcoder <ashleyrolfe954@msn.com>
2022-11-03 09:10:07 +0000

    added veiw to views.py

--- booking/urls.py
+++ booking/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('news/', views.News.as_view(), name="news"),
     path('booking/', views.Create_Booking.as_view(), name="booking"),
     path('booking/<item_id>/edit/', views.edit_booking, name='edit_booking'),
-    path('delete/<item_id>', views.delete_booking, name='delete'),
+    path('booking/<item_id>/delete/', views.delete_booking, name='delete_booking'),
 ]
